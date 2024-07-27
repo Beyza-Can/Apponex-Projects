fc19f64bf0c0eaeb06600286ffbb9c7e3e2e9c95
Beyza Cankurtaran <126654906+Beyza-Can@users.noreply.github.com>
2024-07-27 03:35:38 +0300

    Add files via upload

---
+++ Ecommerce Website/comp/footer.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import './footer.css'
+import { FaPiggyBank, FaShippingFast, FaHeadphonesAlt, FaWallet} from 'react-icons/fa';
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+        <div className='container'>
+            <div className='left-box'>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaPiggyBank />
+                    </div>
+                    <div className='detail'>
+                        <h3>Great Saving</h3>
+                        <p>Lorem ipsum dolor sit amet,</p>
+                    </div>
+                </div>
+                    <div className='box'>
+                    <div className='icon_box'>
+                        <FaShippingFast />
+                    </div>
+                    <div className='detail'>
+                        <h3>free delivery</h3>
+                        <p>Lorem ipsum dolor sit amet,</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaHeadphonesAlt />
+                    </div>
+                    <div className='detail'>
+                        <h3>24X7 support</h3>
+                        <p>Lorem ipsum dolor sit amet,</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaWallet />
+                    </div>
+                    <div className='detail'>
+                        <h3>money back</h3>
+                        <p>Lorem ipsum dolor sit amet,</p>
+                    </div>
+                </div>
+            </div>
+            <div className='right_box'>
+                <div className='header'>
+                    <img src='image/logo.webp' alt=''></img>
+                    <p>Techayo is an e-commerce platform that offers the latest technology products at affordable prices 
+                        and always prioritizes customer satisfaction. With our innovative and dynamic structure, 
+                        we are at your service with a wide range of products that will meet all your technology needs. 
+                        As Techayo, we are committed to providing quality products, fast delivery and a reliable shopping experience. 
+                        Our customer support team is ready to assist you at every stage of your shopping. 
+                        Techayo is moving forward with the goal of becoming the indispensable address for technology enthusiasts.</p>
+                </div>
+                <div className='bottom'>
+                    <div className='box'>
+                        <h3>Your Account</h3>
+                        <ul>
+                            <li>About us</li>
+                            <li>Account</li>
+                            <li>Payment</li>
+                            <li>sales</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>products</h3>
+                        <ul>
+                            <li>Delivery</li>
+                            <li>Track Oder</li>
+                            <li>New product</li>
+                            <li>old product</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>contact us</h3>
+                        <ul>
+                            <li>4005, Silver Business PointIndia</li>
+                            <li>+(012)99999999</li>
+                            <li>info@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer
